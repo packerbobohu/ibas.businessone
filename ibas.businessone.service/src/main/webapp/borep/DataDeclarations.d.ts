57a3f46366efc9a2c67e589645d0464810f8c1cc
niurenzhu <niuren.zhu@icloud.com>
2017-06-24 00:26:48 +0800

    声明类型

---
+++ ibas.businessone.service/src/main/webapp/borep/DataDeclarations.d.ts
@@ -0,0 +1,24 @@
+﻿/**
+ * @license
+ * Copyright color-coding studio. All Rights Reserved.
+ *
+ * Use of this source code is governed by an Apache License, Version 2.0
+ * that can be found in the LICENSE file at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/** ibas的java端数据声明 */
+
+/** 操作消息 */
+export interface DataDeclaration {
+    /** 数据类型 */
+    type: string;
+}
+/** 用户公司 */
+export interface UserCompany extends DataDeclaration {
+    /** 用户 */
+    User: string;
+    /** 公司 */
+    Company: string;
+    /** 地址 */
+    Url: string;
+}
