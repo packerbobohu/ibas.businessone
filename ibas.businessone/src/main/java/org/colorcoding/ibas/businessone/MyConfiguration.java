828269e925cc1ab2ca261658d7d791c689ed02c0
牛加人等于朱 <niuren.zhu@icloud.com>
2017-11-23 10:53:57 +0800

    添加文件管理功能，及统一代码

--- ibas.businessone/src/main/java/org/colorcoding/ibas/businessone/MyConfiguration.java
+++ ibas.businessone/src/main/java/org/colorcoding/ibas/businessone/MyConfiguration.java
@@ -30,6 +30,10 @@ public class MyConfiguration extends org.colorcoding.ibas.bobas.MyConfiguration 
 	 */
 	public static final String NAMESPACE_SERVICE = NAMESPACE_ROOT + "service";
 	/**
+	 * 配置项目-文件文件夹
+	 */
+	public final static String CONFIG_ITEM_BUSINESSONE_FILE_FOLDER = "B1FileFolder";
+	/**
 	 * 变量名称，公司地址
 	 */
 	public static final String PARAMETER_NAME_COMPANY_URL = "${Url}";
